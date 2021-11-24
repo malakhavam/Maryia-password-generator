5ce8e1f03972857ac1e7bbe40bc06261850f7866
Maryia <malakhava@yahoo.com>
2021-11-24 13:14:21 -0500

    restructuring js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment code here
+var minimumCount = 0;
+var passLength = 0;
+var minNumbers = "";
+var minLowerCases = "";
+var minUpperCases = "";
+var minSpecialCharacters = "";
+var randomPassword = "";
 
-const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+var lowerCases = "qwertyuiopasdfghjklzxcvbnm".split("");
+var upperCases = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
+var numbers = "1234567890".split("");
+
+// Array to hold characters
+var prePassword = [];
+var MasterPassword = [];
 
 // Get references to the #generate element
 
@@ -11,102 +25,120 @@ const generateButton = document.getElementById("generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 
 generateButton.addEventListener("click", writePassword);
 
-// Window prompts
+// Function to generate password
 
 function generatePassword() {
-  var passwordLength = prompt("What would you like to be your password lenght? It must be between 8 and 128 characters.");
 
+  function passwordLength() {
+
+    // Window prompts
+
+    passLength = prompt("What would you like to be your password lenght? It must be between 8 and 128 characters.");
+
+  if (passLength < 8 || passLength > 128) {
+    alert("Please, choose amount of characters between 8 and 128");
+    return passwordLength() 
+    }
+  console.log(passLength)
+
+  }
+    passwordLength(); 
+  
   var numbers = confirm("Would you like to have numbers in your password?");
+   
+   if (numbers === true) {
+   var minNumbers = numbers[Math.floor(Math.random() * numbers.length)];
 
-  var lowerCases = confirm("Would you like to have lower case letters in your password?");
+    prePassword.push(minNumbers);
+    
+    MasterPassword = MasterPassword.concat(numbers)
+    minimumCount++;
+  }
+  
+  console.log(minNumbers)
+    
+    var lowerCases = confirm("Would you like to have lower case letters in your password?");
 
-  var upperCases = confirm("Would you like to have upper case letters in your password?");
+    if (lowerCases === true) {
+     var minLowerCases = lowerCases[Math.floor(Math.random() * lowerCases.length)];
 
-  var special = confirm("Would you like to have special characters in your password?");
-
- 
-  var minimumCount = 0;
-
-  var minNumbers = "";
-  var minLowerCases = "";
-  var minUpperCases = "";
-  var minSpecialCharacters = "";
-
-
-  // Functions
-  var functionArray = {
-    getNumbers: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-    },
-
-    getLowerCases: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-    },
-
-    getUpperCases: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-    },
-
-    getSpecialCharacters: function() {
-      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+      prePassword.push(minLowerCases);
+    
+      MasterPassword = MasterPassword.concat(lowerCases)
+      minimumCount = minimumCount+1;
+  
     }
 
-};
+    console.log(minLowerCases)
+  
+    var upperCases = confirm("Would you like to have upper case letters in your password?");
 
-  // User selections
+    if (upperCases === true) {
+     var minUpperCases = upperCases[Math.floor(Math.random() * upperCases.length)];
 
-  if (numbers === true) {
-    minNumbers = functionArray.getNumbers();
-    minimumCount++;
+      prePassword.push(minUpperCases);
+    
+      MasterPassword = MasterPassword.concat(lowerCases)
+      minimumCount = minimumCount+1;
+       
+    }
 
+    console.log(minUpperCases)
+  
+    var special = confirm("Would you like to have special characters in your password?");
+
+    if (special === true) {
+     var minSpecialCharacters = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+      prePassword.push(minSpecialCharacters);
+    
+      MasterPassword = MasterPassword.concat(specialCharacters)
+      minimumCount = minimumCount+1;
+
+    }
+
+    console.log(minSpecialCharacters)
+
+    if (minimumCount === 0){
+      alert("Please choose at least one group of the parameters")
+      return passwordLength()
   }
 
-  if (lowerCases === true) {
-    minLowerCases = functionArray.getLowerCases();
-    minimumCount++;
+  console.log(prePassword)
+  console.log(MasterPassword)
 
+ // random characters in the password
+
+ var difference = passLength-minimumCount
+
+ console.log(difference)
+
+ for (i = 0; i < difference; i++) {
+  var randomPassword = MasterPassword[Math.floor(Math.random() * MasterPassword.length)];
+  prePassword.push(randomPassword);
+
+  var newRandomPassword = prePassword.join("");
+  
+ }
+
+ return newRandomPassword
+ 
   }
 
-  if (upperCases === true) {
-    minUpperCases = functionArray.getUpperCases();
-    minimumCount++;
 
-  }
 
-  if (special === true) {
-    minSpecialCharacters = functionArray.getSpecialCharacters();
-    minimumCount++;
-
-  }
-
-  // empty string 
-  var randomPassword = "";
-
-  // random characters in the password
-
-  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
-    var randomNumber = Math.floor(Math.random() * 4);
-
-    randomPassword += randomNumber;
-
-  }
+ 
 
   
-  randomPassword += minNumbers;
-  randomPassword += minLowerCases;
-  randomPassword += minUpperCases;
-  randomPassword += minSpecialCharacters;
+ 
 
 
-  return randomPassword;
+  
 
-}
